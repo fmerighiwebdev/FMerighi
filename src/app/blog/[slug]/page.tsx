72e90c3686528f0651c59e05e311e9ca5a2fefd5
francescomerighi1202 <francescomerighi61@gmail.com>
2025-02-24 16:22:34 +0100

    Bug fix

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,44 +1,74 @@
 import { notFound } from "next/navigation";
 import { CustomMDX } from "@/components/mdx";
 import { getPosts } from "@/app/utils/utils";
-import { AvatarGroup, Button, Column, Heading, Row, Text } from "@/once-ui/components";
+import {
+  AvatarGroup,
+  Button,
+  Column,
+  Heading,
+  Row,
+  Text,
+} from "@/once-ui/components";
 import { baseURL } from "@/app/resources";
 import { person } from "@/app/resources/content";
 import { formatDate } from "@/app/utils/formatDate";
 
 interface BlogParams {
-  params: {
-    slug: string;
-  };
+  params: Promise<{ slug: string }>;
 }
 
-export default function Blog({ params }: BlogParams) {
-  let post = getPosts(["src", "app", "blog", "posts"]).find((post) => post.slug === params.slug);
+export async function generateMetadata({ params }: BlogParams) {
+    const { slug } = await params;
+    let post = getPosts(["src", "app", "blog", "posts"]).find(
+      (post) => post.slug === slug
+    );
 
-  if (!post) {
-    notFound();
-  }
+    if (!post) {
+      notFound()
+    }
 
-  const avatars =
-    post.metadata.team?.map((person) => ({
-      src: person.avatar,
-    })) || [];
+    return {
+      title: post.metadata.title,
+      description: post.metadata.summary,
+    };
+}
 
-  return (
-    <Column as="section" maxWidth="xs" gap="l">
-      <Button href="/blog" weight="default" variant="tertiary" size="s" prefixIcon="chevronLeft">
-        Posts
-      </Button>
-      <Heading variant="display-strong-s">{post.metadata.title}</Heading>
-      <Row gap="12" vertical="center">
-        {avatars.length > 0 && <AvatarGroup size="s" avatars={avatars} />}
-        <Text variant="body-default-s" onBackground="neutral-weak">
-          {formatDate(post.metadata.publishedAt)}
-        </Text>
-      </Row>
-      <Column as="article" fillWidth>
-        <CustomMDX source={post.content} />
+export default async function Blog({ params }: BlogParams) {
+    const { slug } = await params;
+    let post = getPosts(["src", "app", "blog", "posts"]).find(
+      (post) => post.slug === slug
+    );
+
+    if (!post) {
+      notFound();
+    }
+
+    const avatars =
+      post.metadata.team?.map((person) => ({
+        src: person.avatar,
+      })) || [];
+
+    return (
+      <Column as="section" maxWidth="xs" gap="l">
+        <Button
+          href="/blog"
+          weight="default"
+          variant="tertiary"
+          size="s"
+          prefixIcon="chevronLeft"
+        >
+          Posts
+        </Button>
+        <Heading variant="display-strong-s">{post.metadata.title}</Heading>
+        <Row gap="12" vertical="center">
+          {avatars.length > 0 && <AvatarGroup size="s" avatars={avatars} />}
+          <Text variant="body-default-s" onBackground="neutral-weak">
+            {formatDate(post.metadata.publishedAt)}
+          </Text>
+        </Row>
+        <Column as="article" fillWidth>
+          <CustomMDX source={post.content} />
+        </Column>
       </Column>
-    </Column>
-  );
+    );
 }
